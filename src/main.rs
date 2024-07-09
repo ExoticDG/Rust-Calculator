601620af9e13e63eb956fa955182291836e5c459
ExoticDG <puppy7693@gmail.com>
2024-07-09 12:18:53 -0500

    V3 IT WORKS!!!

--- src/main.rs
+++ src/main.rs
@@ -14,30 +14,61 @@
 use std::io;
 
 
-fn main() {
-    println!("Please type something, or x to escape:");
-    let mut num1:i128;
-    let mut sym = String::new();
-    let mut num2:i128;
-    let mut answer:i128;
 
+
+
+fn main() {
+
+    let mut sym = String::new();
+    let mut num1_string = String::new();
+    let mut num2_string = String::new();
+
+    println!("Please type the first number and hit enter.");
+
+        num1_string.clear();
+        io::stdin().read_line(&mut num1_string).unwrap();
+        num1_string = num1_string.trim().to_string();
+        let num1 = num1_string.parse::<i128>().unwrap();
+
+    println!("Please type the second number and hit enter.");
+
+        num2_string.clear();
+        io::stdin().read_line(&mut num2_string).unwrap();
+        num2_string = num2_string.trim().to_string();
+        let num2: i128 = num2_string.parse().unwrap();
+
+    println!("Please type a math symbol (* , / , + , -) and hit enter.");
+
+        sym.clear();
+        io::stdin().read_line(&mut sym).unwrap(); 
+        sym = sym.trim().to_string();
+
+    solve(num1,sym,num2);
+
+
+}
+
+    fn solve (num1: i128,sym:String,num2:i128){
+        let mut answer:i128=0;   
+        
         if sym == "*" {
-            let answer = num1*num2;   
+            answer = num1*num2;   
         }
         else if sym == "/" {
-            let answer = num1/num2;
+            answer = num1/num2;
         }
         else if sym == "-"  {
-            let answer = num1-num2;
+            answer = num1-num2;
             
         }
         else if sym == "+" {
-            let answer = num1+num2;
+            answer = num1+num2;
         }
         else {
             println!("Sorry. This is not a viable problem. Try again.")
         };
-    println!("The answer is {}", answer);
+        
+        println!("The answer is {}", answer);
 
     }
 
